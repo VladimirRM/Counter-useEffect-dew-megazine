9bc2d301a64b6cbf243e9733ba2dbcf9ff9d7dd1
Vladimir RM <shvdrummer@gmail.com>
2023-01-13 17:47:02 +0200

    check timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import { Button } from "react-bootstrap";
 import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [intervalValue, setIntervalValue] = useState();
 
-const handleIntervalClick =()=>{
-  
-}
+  function Timer({ intervalValue }) {
+    const [value, setValue] = useState(0);
+
+    useEffect =
+      (() => {
+         const intervalId = setInterval(() => {
+            setValue((value) => value + 1);
+        }, intervalValue);
+        return()=>{
+             clearInterval()
+        }
+      },
+      [intervalValue]);
+    return (
+      <div className="container">
+        <h1>Timer</h1>
+        <h2>{value}</h2>
+      </div>
+    );
+  }
+
+  const handleIntervalClick = () => {
+    setIntervalValue();
+  };
 
   return (
     <div className="container">
